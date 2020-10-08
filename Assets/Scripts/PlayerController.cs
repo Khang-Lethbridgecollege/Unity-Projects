4fce57ab3264d429dad0c9cc128ee6ba7b4f500d
Khang-Lethbridgecollege <72067596+Khang-Lethbridgecollege@users.noreply.github.com>
2020-10-07 22:44:15 -0600

    sandbox
    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    private Rigidbody rb;
+    private int count;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0; //Set the score is at 0 when the game starts 
+        SetCountText();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveSpeed = 10;
+        //Define the speed at which the object moves.
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //Get the value of the Horizontal input axis.
+
+        float verticalInput = Input.GetAxis("Vertical");
+        //Get the value of the Vertical input axis.
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+     
+        rb.AddForce(movement *speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false); //The pick ups will be deactive when interact with player 
+            count = count + 1;          //The point increases for each collection 
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 9)
+        {
+            winText.text = "You Win! Congratulations!";
+        }
+    }
+}
---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public Transform SpawnPoint;
+    public GameObject Prefab;
+    void OnTriggerEnter()
+    {
+    
+Instantiate(Prefab, SpawnPoint.position, SpawnPoint.rotation);
+        Destroy(gameObject);
+    }
+}
